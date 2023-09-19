2857aa048979d065fd7ba515a2cd5cfe61aca74b
Victor Zagnitko <victor.zagnitko@gmail.com>
2023-09-19 17:21:46 +0300

    Initial commit

---
+++ src/main/java/com/example/testrdsmysql/TestRdsMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.testrdsmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRdsMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRdsMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testrdsmysql/TestRdsMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testrdsmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRdsMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
